b7793935268462771adcdc87f1b2e7e36a7cd3fd
yash chandrani <ychandr@ncsu.edu>
2024-07-18 18:12:32 -0400

    API for To Do List

--- api/views.py
+++ api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User
+from .models import Task
+from .serializers import UserSerializer, TaskSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key})
+    return Response(serializer.errors)
+
+@api_view(['POST'])
+def login(request):
+    from django.contrib.auth import authenticate
+    user = authenticate(username=request.data['username'], password=request.data['password'])
+    if user:
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+    return Response({'error': 'Invalid Credentials'})
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
