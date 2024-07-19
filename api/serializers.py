c22a7bb0cb4eb1e59521f0cfa209822d5524fd59
yash chandrani <ychandr@ncsu.edu>
2024-07-19 07:34:13 -0400

    Validations

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Task
+from django.core.exceptions import ValidationError
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(username=validated_data['username'], password=validated_data['password'])
@@ -16,3 +22,8 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'title', 'completed']
+
+    def validate_title(self, value):
+        if len(value) == 0:
+            raise serializers.ValidationError("Title cannot be empty.")
+        return value
